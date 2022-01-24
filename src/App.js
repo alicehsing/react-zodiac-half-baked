00873b66cf2b188da2df6a96d27b0710803d3750
Alice Hsing <alicehsing@gmail.com>
2022-01-24 15:32:17 -0800

    added line14-16 in Main.js and added ClassName for div in ZodiacCard.js

--- src/App.js
+++ src/App.js
@@ -4,18 +4,17 @@ import Main from './Main/Main.js'
 import Footer from './Footer/Footer.js'
 import './App.css'
 {/* import the zodiac array */}
-import { zodiac } from './data'
+import { zodiac } from './data.js'
 
 function App() {
   return (
     <div className="App">
       {/* import and use the Header. Main, and Footer components here */}
       {/* the Header component needs a string passed in as the `name` prop */}
-      <Header
-        name='Alice Hsing'/>
+      <Header name='Alice Hsing'/>
       {/* pass the zodiac array imported above to the Main component as a `zodiacs` prop */}
       <Main
-        zodiacs='{ zodiac }'/>
+        zodiacs={zodiac}/>
 
       {/* the Footer component needs a year passed in as the `year` prop */}
       <Footer year='1986'/>
