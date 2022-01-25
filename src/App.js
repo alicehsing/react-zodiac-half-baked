bc8e03be5a34f71fd74e8e2a417fad8e7a5366ad
Alice Hsing <alicehsing@gmail.com>
2022-01-24 16:08:57 -0800

    clean up code

--- src/App.js
+++ src/App.js
@@ -12,12 +12,13 @@ function App() {
       {/* import and use the Header. Main, and Footer components here */}
       {/* the Header component needs a string passed in as the `name` prop */}
       <Header name='Alice Hsing'/>
+      
       {/* pass the zodiac array imported above to the Main component as a `zodiacs` prop */}
       <Main
         zodiacs={zodiac}/>
 
       {/* the Footer component needs a year passed in as the `year` prop */}
-      <Footer year='1986'/>
+      <Footer year='2022'/>
   
     </div>
   )
--- src/ZodiacCard/ZodiacCard.js
+++ src/ZodiacCard/ZodiacCard.js
@@ -4,8 +4,7 @@ export default function ZodiacCard(props) {
   {/* in this component,  use the `name` and `dates` props to render the name and dates of the sign, as well as load the correct image  from the public directory */}
 
   return <div className="zodiac-card">
-    {props.name},
-    {props.dates} 
+    {props.name}: {props.dates} 
     <img src={`/images/${props.name}.png`}/>
   </div>
 }
